afdcc9ed8938292f6c3dae76ecfe4b7c3a228696
arisamuto <cookiemonster.ca17@gmail.com>
2019-06-06 08:33:41 +0900

    struggling

--- assignment6/TableViewController.swift
+++ assignment6/TableViewController.swift
@@ -9,44 +9,47 @@
 import UIKit
 
 class TableViewController: UITableViewController {
-  
+   
     var label1: [Int]!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         let iValue = Int.random(in: 1 ..< 100)
         print (iValue)
         
-        for i in 0 ..< 19 {
+        for i in 0 ..< 20 {
             label1.append(iValue)
             print(label1)
+            
         }
+        
         tableView.register(UINib(nibName: "ListTableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
-    }
     
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-//セクション数を指定
+    //セクション数を指定
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-//セルの個数を指定
+    //セルの個数を指定
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 20
     }
-//セルの中身の表示の仕方
+    //セルの中身の表示の仕方
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ListTableViewCell
         cell.number.text = String(label1[indexPath.row])
-
+        
         return cell
     }
-
+    
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
- 
+    
 }
